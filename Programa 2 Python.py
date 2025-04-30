e00346687235fd5f06aac7e9c749920b9b8c908a
Morales Ximena <ximenamoralesp09@gmail.com>
2025-04-29 18:59:12 -0600

    Update Programa 2 Python.py

--- Programa 2 Python.py
+++ Programa 2 Python.py
@@ -4,9 +4,10 @@ if numero %2==0:
 else:
     print("El numero es impar.")
     cantidad=int(input("¿Cuántos números pares desea ver?: "))
-    contador = 0
-    i=1 
-    while contador<cantidad:
-        if i%2==0:print("Par número"<contador+1,":",i) 
-        contador+=1 
-        i+=1 
+contador=0
+i=1 
+while contador<cantidad:
+        if i%2==0:
+            print(f"Par número{contador+1}:{i}") 
+            contador+=1 
+        i+=1
