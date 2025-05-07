36337d39899f910079d8ff21afdfd4bc8766b593
Morales Ximena <ximenamoralesp09@gmail.com>
2025-05-06 20:43:25 -0600

    Hola y edad Ximena.py

--- Programa Hola Ximena.py
+++ Programa Hola Ximena.py
@@ -4,18 +4,28 @@ def saludar():
     nombre = entrada.get()
     etiqueta_resultado.confi(text=f"Hola{nombre}")
     
+    edad = entrada.get()
+    etiqueta_resultado.confi(text=f"Tienes{edad}años")
+    
     ventana = tk.Tk()
-    ventana.title("Saludo")
+    ventana.title("Saludo") 
+    ventana.title("Edad") 
     ventana.geometry("300x150")
     
     etiqueta = tk.Label(ventana, text="Ingresa tu nombre:")
+    etiqueta.pack()
+    
+    etiqueta = tk.Label(ventana, text="Ingresa tu edad:")
     etiqueta.pack()
     
     entrada = tk.Entry(ventana)
     entrada.pack()
     
     boton = tk.Button(ventana, text="Saludar", command=saludar)
-    boton.pack()
+    boton.pack() 
+
+    boton = tk.Button(ventana, text=" Mostrar edad", command=mostrar_edad)
+    boton.pack() 
     
     etiqueta_resultado = tk.Label(ventana, text="")
     etiqueta_resultado.pack()
